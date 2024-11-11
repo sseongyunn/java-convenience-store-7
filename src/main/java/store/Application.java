28fe619050cebdb816967e385bfce10952582cdc
sseongyunn <sseongyunn@korea.ac.kr>
2024-11-11 23:12:23 +0900

    feat: Application 클래스 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,22 @@
 package store;
 
+import java.io.IOException;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Manager manager = new Manager();
+        PosSystem posMachine = new PosSystem(manager.itemsList);
+        try {
+            manager.setupStore();
+
+            do{
+                manager.showItems();
+                posMachine.processCheckout(manager.itemsList, manager.promotionsList);
+                manager.updateInventory(posMachine.getCartCurrentInventory());
+//                posMachine.printReceipt();
+            }while(posMachine.askContinue());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
